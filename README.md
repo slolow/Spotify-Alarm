332e38ada8bc6610bc7b6dd85688e24653eb226f
slolow <laszlostarost@googlemail.com>
2020-10-20 00:59:59 +0200

    spotify-alarm 1.0

---
+++ README.md
@@ -0,0 +1,20 @@
+app.py:
+  - This is a Spotify alarm clock
+  - how to on Windows 10:
+    - getting started:
+      - needed: python and pip installed
+      - run pip install -r requirements.txt
+      - go to [Dashboard for Spotify developer](https://developer.spotify.com/dashboard/login) and login with your spotify account
+      - create new app on Dashboard
+      - after you have created a new app click on 'edit settings' button and add the google homepage of your country to Redirect URIs
+    - run file:
+      - In terminal command move to folder where you saved the file and type:
+        - set SPOTIPY_CLIENT_ID=Enter your CLient Id here
+        - set SPOTIPY_CLIENT_SECRET=Enter your CLient Secret here
+        - set SPOTIPY_REDIRECT_URI=Enter your Redirect URL here
+        - python app.py 'your Spotify username' 'the device name you want to play the music on'
+        - If Running this script the first you'll need to authorize Spotify and you'll be redirect to an URL copy that URL in the terminal command.
+          This information will be saved in a file .cache-"your username" and will not be asked the next time.
+
+spotify-app.py:
+  found on https://dev.to/arvindmehairjan/how-to-play-spotify-songs-and-show-the-album-art-using-spotipy-library-and-python-5eki
---
+++ app.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import spotipy
+from json.decoder import JSONDecodeError
+from datetime import datetime
+
+
+class Alarm:
+    def __init__(self):
+        self.testSuccess = False
+
+    def setAlarmTime(self):
+        self.alarm_time = input('Enter alarm time \'HH:MM\': ') + ':00'
+
+    def connectToSpotify(self, username, scope):
+        try:
+            self.token = spotipy.util.prompt_for_user_token(username, scope)
+        except (AttributeError, JSONDecodeError):
+            os.remove(f".cache-{username}")
+            self.token = spotipy.util.prompt_for_user_token(username, scope)
+        return self.token
+
+    # test if the right album was found
+    def test(self, deviceID, albumUri):
+        self.deviceID = deviceID
+        self.albumUri = albumUri
+        self.startMusic()
+        # check test
+        self.rightAlbum = input('Is the right album playing? y or n? ')
+        if self.rightAlbum == 'y':
+            self.pauseMusic()
+            self.testSuccess = True
+
+    def isAlarmTime(self):
+        self.current_time = datetime.now().strftime("%H:%M:%S")
+        if self.current_time == self.alarm_time:
+            return True
+
+    def startMusic(self):
+        spotifyObject.start_playback(self.deviceID, self.albumUri)
+
+    def pauseMusic(self):
+        spotifyObject.pause_playback(self.deviceID)
+
+
+def getDiviceID(devices, diviceName):
+    for device in devices:
+        if device['name'] == deviceName:
+            deviceID = device['id']
+    return deviceID
+
+
+# Get the username and devicename from terminal
+username = sys.argv[1]
+deviceName = sys.argv[2]
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+
+# set alarm
+alarm = Alarm()
+alarm.setAlarmTime()
+token = alarm.connectToSpotify(username, scope)
+
+# search for the device id to play the music on
+spotifyObject = spotipy.Spotify(auth=token)
+devices = spotifyObject.devices()['devices']
+deviceID = getDiviceID(devices, deviceName)
+
+# search for an album as long as the user found the right one
+while True:
+    # search for an album
+    searchQuery = input('Enter the album you\'re looking for: ' )
+    searchResults = spotifyObject.search(searchQuery,1,0,'album')
+    albumUri = searchResults['albums']['items'][-1]['uri']
+
+    alarm.test(deviceID, albumUri)
+    if alarm.testSuccess:
+        break
+
+# check if it is alarm time
+while True:
+    if alarm.isAlarmTime():
+        alarm.startMusic()
+        break
