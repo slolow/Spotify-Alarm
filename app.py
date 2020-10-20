46be2e3e6718b0f68ed0141c024e072ec60818ce
slolow <laszlostarost@googlemail.com>
2020-10-21 01:41:34 +0200

    Update: Add display function when alarm on. Turn alarm off if music paused or 1 hour elapsed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import sys
 import spotipy
 import time
+import webbrowser
 from json.decoder import JSONDecodeError
 from datetime import datetime
 
@@ -9,11 +10,13 @@ from datetime import datetime
 class Alarm:
     def __init__(self):
         self.testSuccess = False
+        self.timer = 0
 
     def setAlarmTime(self):
         self.alarm_time = input('Enter alarm time \'HH:MM\': ')
 
     def connectToSpotify(self, username, scope):
+        self.username = username
         try:
             self.token = spotipy.util.prompt_for_user_token(username, scope)
         except (AttributeError, JSONDecodeError):
@@ -34,8 +37,8 @@ class Alarm:
             self.testSuccess = True
 
     def isAlarmTime(self):
-        self.current_time = datetime.now().strftime("%H:%M")
-        if self.current_time == self.alarm_time:
+        self.currentTime = datetime.now().strftime("%H:%M")
+        if self.currentTime == self.alarm_time:
             return True
 
     def startMusic(self):
@@ -44,6 +47,30 @@ class Alarm:
     def pauseMusic(self):
         spotifyObject.pause_playback(self.deviceID)
 
+    def display(self):
+        print(f'Good morning {self.username}!')
+        # display album cover
+        self.album = spotifyObject.current_user_playing_track()
+        self.albumCover = self.album['item']['album']['images'][0]['url']
+        webbrowser.open(self.albumCover)
+        
+        # While Alarm is on display the time every minute
+        while True:
+            self.currentTime = datetime.now().strftime("%H:%M")
+            print(self.currentTime)
+            time.sleep(60)
+            if self.endAlarm():
+                break
+
+    def endAlarm(self):
+        # end Alarm if spotify paused or 1 hour has past
+        self.timer += 1
+        self.isPlaying = self.album['is_playing']
+
+        if self.isPlaying and self.timer < 60:
+            return False
+        else:
+            return True
 
 def getDiviceID(devices, diviceName):
     for device in devices:
@@ -52,35 +79,37 @@ def getDiviceID(devices, diviceName):
     return deviceID
 
 
-# Get the username and devicename from terminal
-username = sys.argv[1]
-deviceName = sys.argv[2]
-scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+if __name__ == '__main__':
+    # Get the username and devicename from terminal
+    username = sys.argv[1]
+    deviceName = sys.argv[2]
+    scope = 'user-read-private user-read-playback-state user-modify-playback-state'
 
-# set alarm
-alarm = Alarm()
-alarm.setAlarmTime()
-token = alarm.connectToSpotify(username, scope)
+    # set alarm
+    alarm = Alarm()
+    alarm.setAlarmTime()
+    token = alarm.connectToSpotify(username, scope)
 
-# search for the device id to play the music on
-spotifyObject = spotipy.Spotify(auth=token)
-devices = spotifyObject.devices()['devices']
-deviceID = getDiviceID(devices, deviceName)
+    # search for the device id to play the music on
+    spotifyObject = spotipy.Spotify(auth=token)
+    devices = spotifyObject.devices()['devices']
+    deviceID = getDiviceID(devices, deviceName)
 
-# search for an album as long as the user found the right one
-while True:
-    # search for an album
-    searchQuery = input('Enter the album you\'re looking for: ' )
-    searchResults = spotifyObject.search(searchQuery,1,0,'album')
-    albumUri = searchResults['albums']['items'][-1]['uri']
+    # search for an album as long as the user found the right one
+    while True:
+        # search for an album
+        searchQuery = input('Enter the album you\'re looking for: ' )
+        searchResults = spotifyObject.search(searchQuery,1,0,'album')
+        albumUri = searchResults['albums']['items'][-1]['uri']
 
-    alarm.test(deviceID, albumUri)
-    if alarm.testSuccess:
-        break
+        alarm.test(deviceID, albumUri)
+        if alarm.testSuccess:
+            break
 
-# check if it is alarm time
-while True:
-    time.sleep(30)
-    if alarm.isAlarmTime():
-        alarm.startMusic()
-        break
+    # check if it is alarm time every 30 seconds
+    while True:
+        time.sleep(1)
+        if alarm.isAlarmTime():
+            alarm.startMusic()
+            alarm.display()
+            break
